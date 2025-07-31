e11856350a4fccf4e5f389d408decc002c1030c5
Zark <khanzark37@gmail.com>
2025-07-31 13:23:43 +0500

    - Worked on fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:sample_task/feature/ble/presentation/block/ble_bloc.dart';
-import 'package:sample_task/feature/ble/presentation/block/ble_event.dart';
-import 'package:sample_task/feature/home_screens/presentation/pages/shot_analysis_page.dart';
 import 'package:sample_task/feature/splash/splash_screen.dart';
 import 'core/di/injection_container.dart' as di;
 import 'core/constants/app_strings.dart';
